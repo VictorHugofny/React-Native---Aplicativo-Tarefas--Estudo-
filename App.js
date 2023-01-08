29e2fb2d40bf484a287068324a909a85d155f1a2
Victor Hugo <victorhugofny@gmail.com>
2023-01-08 12:20:53 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,22 +1,82 @@
-import React from 'react';
-import {View, Text, StyleSheet} from "react-native"
+import React, {useState} from 'react';
+import {
+  View, 
+  Text, 
+  StyleSheet, 
+  TouchableOpacity, 
+  TextInput
+} from "react-native"
+
+import {FontAwesome} from '@expo/vector-icons';
+import { disableExpoCliLogging } from 'expo/build/logs/Logs';
 
 export default function App(){
+  const [tarefa, setTarefa] = useState("");
+
+  function handleAdd(){
+    alert (tarefa)
+  }
+
   return(
     <View style = {styles.container}>
-      <Text>Teste</Text>
-      <Text>Teste</Text>
-      <Text>Teste</Text>
-      <Text>Teste</Text>
-      <Text>Teste</Text>
+      <Text style = {styles.title}>Teste</Text>
+
+      <View style = {styles.containerInput}>
+        <TextInput 
+          placeholder='Digite sua tarefa' 
+          style = {styles.input}
+          value = {tarefa}
+          onChangeText = {(text)=> setTarefa(text)}
+        />
+
+        <TouchableOpacity style = {styles.buttonAdd} onPress = {handleAdd}>
+          <FontAwesome name="plus" size={20} color="#fff"/>
+        </TouchableOpacity>
+      </View>
+
+      
     </View>
   )
 }
 
+//criando estilos
 const styles = StyleSheet.create({
   container:{
     flex: 1,
-    backgroundColor: "#1212",
-    paddingTop: 38
+    backgroundColor: "#22272e",
+    paddingTop: 38,
+  },
+  title:{
+    fontWeight: "bold",
+    fontSize: 24,
+    color: "#FFF",
+    marginTop: "5%",
+    paddingStart: "5%",
+    marginBottom: 12
+  },
+  containerInput:{
+    flexDirection: "row",
+    width: "100%",
+    height: 44,
+    alignItems: "center",
+    justifyContent: "center",
+    marginBottom: 22
+  },
+  input:{
+    width: "75%",
+    backgroundColor: "#FBFBFB",
+    height: 44,
+    borderRadius: 4,
+    paddingHorizontal: 8,
+  },
+  buttonAdd:{
+    width:"15%",
+    height: 44,
+    backgroundColor: "#73f7ff",
+    marginLeft: 8,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 4
   }
+  
 })
